66ceeda36a5fb220cd655ae944e8ca15a4e9e54c
cutcutcutcut <bekhterev98@mail.ru>
2021-03-28 18:36:31 +0400

    added SpringConfig and DispatcherServletInitializer

--- src/main/java/org/cheeseapp/Controller.java
+++ src/main/java/org/cheeseapp/Controller.java
@@ -2,8 +2,9 @@ package org.cheeseapp;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @org.springframework.stereotype.Controller
 public class Controller {
-    //sout
+    //@GetMapping
 
 }
