1774cdedc685fde5e5e13953dd12c768965a0df5
Egor Novichkov <61648756+spelldone@users.noreply.github.com>
2021-04-11 15:58:27 +0400

    feat: add registration

--- src/main/java/org/cheeseapp/Application.java
+++ src/main/java/org/cheeseapp/Application.java
@@ -1,5 +1,6 @@
 package org.cheeseapp;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/org/cheeseapp/controllers/RegistrationController.java
@@ -0,0 +1,37 @@
+package org.cheeseapp.controllers;
+
+
+import org.cheeseapp.domain.Role;
+import org.cheeseapp.domain.User;
+import org.cheeseapp.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping("/registration")
+    public String registration(){
+        return "registration";
+    }
+    @PostMapping("/registration")
+    public String addUser(User user, Map<String,Object> model){
+        User userFromDb=userRepo.findByLogin(user.getLogin());
+         if(userFromDb!=null){
+             model.put("message", "User exists");
+             return "registration";
+         }
+         user.setRoles(Collections.singleton(Role.CLIENT));
+         userRepo.save(user);
+        return "redirect:/login";
+
+
+    }
+}
