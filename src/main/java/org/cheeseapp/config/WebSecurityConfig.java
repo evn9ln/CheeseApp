472bd8b65abab56705c59c13869e626671889e44
cutcutcutcut <bekhterev98@mail.ru>
2021-05-08 17:51:21 +0400

    change sign in, sign up and profile

--- src/main/java/org/cheeseapp/config/WebSecurityConfig.java
+++ src/main/java/org/cheeseapp/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -41,5 +42,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(NoOpPasswordEncoder.getInstance()) //(будущая) шифровка паролей
                 .usersByUsernameQuery("select login, password, active from usr where login=?") //поиск пользователя
                 .authoritiesByUsernameQuery("select u.login, ur.roles from usr u inner join user_role ur on u.id = ur.user_id where u.login=?"); //распределение по ролям (beta)
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers(
+          //static
+          "/img/**",
+                "/css/**",
+                "static/**"
+        );
     }
 }
