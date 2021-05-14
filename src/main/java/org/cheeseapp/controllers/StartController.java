dab8d3c68a05e50d17936cac601d146184f05aa4
cutcutcutcut <bekhterev98@mail.ru>
2021-05-14 20:32:48 +0400

    fixed: sign out button delete: home page, its mapping and links

--- src/main/java/org/cheeseapp/controllers/StartController.java
+++ src/main/java/org/cheeseapp/controllers/StartController.java
@@ -4,16 +4,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class StartController {
-
-    @GetMapping("/home")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="Guest") String name, Model model) {
-        model.addAttribute("name", name);
-        return "home";
-    }
 
     @GetMapping
     public String main(Model model) {
